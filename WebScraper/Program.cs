de24f4e9e4007656c6a0a5f574b482a008a4eae8
Jonah <jonahcole@yahoo.com>
2023-06-23 14:41:47 -0700

    Get all users from thread without users from 'suggested threads'

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -21,6 +21,27 @@ namespace WebScraper
             var username = usernameElement.InnerText;
             Console.WriteLine(username);
 
+            // get date of original post
+            var dateElement = htmlDocument.DocumentNode.SelectSingleNode("//time[@class='u-dt']");
+            var date = dateElement.InnerText;
+            Console.WriteLine(date);
+
+            // get content of original post
+            var contentElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='bbWrapper']");
+            var content = contentElement.InnerText;
+            Console.WriteLine(content);
+
+
+            //try to get all users from thread
+            // dont include users from 'suggested threads' sections
+            var usernamesElement = htmlDocument.DocumentNode.SelectNodes("//div[@class='block block--messages']//a[@class='username ']");
+            foreach (var user in usernamesElement)
+            {
+                var usernames = user.InnerText;
+                Console.WriteLine(usernames);
+            }
+            
+
         }
     }
 }
