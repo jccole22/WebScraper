f624749b9b7742c00371afc2daa57a2cedc09e40
Jonah <jonahcole@yahoo.com>
2023-06-23 14:07:36 -0700

    Add basic code to acess website and scrape username

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main()
+        {
+            // send get request and store the result
+            string url = "https://basenotes.com/threads/need-help-creating-a-cedramber-accord.537211/#post-5924636";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            // turn result into html doc that can be parsed
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // get username of original poster
+            var usernameElement = htmlDocument.DocumentNode.SelectSingleNode("//a[@class='username ']");
+            var username = usernameElement.InnerText;
+            Console.WriteLine(username);
+
+        }
+    }
+}
