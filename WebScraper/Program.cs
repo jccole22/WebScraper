16196d46dbe90c7fe18af62217cfa35bd3fa7e37
Jonah <jonahcole@yahoo.com>
2023-06-26 15:10:34 -0700

    Refactor scraping code into its own class

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,57 +1,20 @@
-﻿using HtmlAgilityPack;
-using System;
-using System.Net.Http;
-
+﻿
 namespace WebScraper
 {
     class Program
-    {
-        static readonly string url = "https://basenotes.com/threads/need-help-creating-a-cedramber-accord.537211/#post-5924636";
-        
+    {    
         static void Main()
         {
-            // send get request and load html file
-            HttpClient httpClient = new();
-            string html = httpClient.GetStringAsync(url).Result;
-            HtmlDocument htmlDocument = new();
-            htmlDocument.LoadHtml(html);
+            //"https://basenotes.com/threads/need-help-creating-a-cedramber-accord.537211/#post-5924636"
+            string url = "https://basenotes.com/threads/argon-gas-aka-winesave-or-bht-for-preserving-acs.526896/#post-5924635";
+            
+            Scraper scraper = new Scraper(url);
+            
 
             // get info from html
-            ScrapeHead(htmlDocument);
-            ScrapeBody(htmlDocument);
+            scraper.ScrapeHead();
+            scraper.ScrapeBody();
          
-        }
-
-
-
-        static void ScrapeHead(HtmlDocument htmlDocument)
-        {
-            var titleNode = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='p-title-value']");
-            string title = titleNode.InnerText;
-            Console.WriteLine($"Title: {title}");
-
-            var authorNode = htmlDocument.DocumentNode.SelectSingleNode("//a[@class='username  u-concealed']");
-            string author = authorNode.InnerText;
-            Console.WriteLine($"Original Poster: {author}");
-
-            var postDateNode = htmlDocument.DocumentNode.SelectSingleNode("//time[@class='u-dt']");
-            string postDate = postDateNode.InnerText;
-            Console.WriteLine($"Post Date: {postDate}\n");
-        }
-
-        static void ScrapeBody(HtmlDocument htmlDocument)
-        {
-            var articleNodes = htmlDocument.DocumentNode.SelectNodes("//article[@class='message message--post js-post js-inlineModContainer  ']");
-
-            foreach (var article in articleNodes)
-            {
-                string author = article.Attributes["data-author"].Value;
-                Console.WriteLine($"Author: {author}");
-                string time = article.SelectSingleNode(".//li[@class='u-concealed']//time[@class='u-dt']").InnerText;
-                Console.WriteLine($"Date: {time}");
-                string message = article.SelectSingleNode(".//div[@class='bbWrapper']").InnerText;
-                Console.WriteLine($"Message: {message}\n");
-            }
         }
     }
 }
---
+++ WebScraper/Scraper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Net.Http;
+
+
+namespace WebScraper
+{
+    public class Scraper
+    {
+        string url { get; set; }
+        HttpClient httpClient = new();
+        string html;
+        HtmlDocument htmlDocument = new();
+
+        public Scraper(string pUrl)
+        {
+            url = pUrl;
+            // send get request and load html file      
+            html = httpClient.GetStringAsync(url).Result;
+            htmlDocument.LoadHtml(html);
+        }
+
+        public void ScrapeHead()
+        {
+            var titleNode = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='p-title-value']");
+            string title = titleNode.InnerText;
+            Console.WriteLine($"Title: {title}");
+
+            var authorNode = htmlDocument.DocumentNode.SelectSingleNode("//a[@class='username  u-concealed']");
+            string author = authorNode.InnerText;
+            Console.WriteLine($"Original Poster: {author}");
+
+            var postDateNode = htmlDocument.DocumentNode.SelectSingleNode("//time[@class='u-dt']");
+            string postDate = postDateNode.InnerText;
+            Console.WriteLine($"Post Date: {postDate}\n");
+        }
+
+        public void ScrapeBody()
+        {
+            var articleNodes = htmlDocument.DocumentNode.SelectNodes("//article[@class='message message--post js-post js-inlineModContainer  ']");
+
+            foreach (var article in articleNodes)
+            {
+                string author = article.Attributes["data-author"].Value;
+                Console.WriteLine($"Author: {author}");
+                string time = article.SelectSingleNode(".//li[@class='u-concealed']//time[@class='u-dt']").InnerText;
+                Console.WriteLine($"Date: {time}");
+                string message = article.SelectSingleNode(".//div[@class='bbWrapper']").InnerText;
+                Console.WriteLine($"Message: {message}\n");
+            }
+        }
+    }
+  
+}
